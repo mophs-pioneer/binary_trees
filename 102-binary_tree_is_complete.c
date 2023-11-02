9d97e4e3f5a221a0cf62e8bb60478d2f36b8cca0
emankware <lucymankware@gmail.com>
2023-11-02 11:57:26 -0700

    binary tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,99 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "binary_trees.h"
 
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
 /**
- * print_t - Stores recursively each level in an array of strings
- *
- * @tree: Pointer to the node to print
- * @offset: Offset to print
- * @depth: Depth of the node
- * @s: Buffer
- *
- * Return: length of printed tree after process
+ * binary_tree_size - counts the number of nodes in the binary tree
+ * @root: pointer to the root node of the tree
+ * Return: Number of nodes in the binary tree
  */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
-{
-	char b[6];
-	int width, left, right, is_left, i;
 
-	if (!tree)
+size_t binary_tree_size(const binary_tree_t *root)
+{
+	if (root == NULL)
 		return (0);
-	is_left = (tree->parent && tree->parent->left == tree);
-	width = sprintf(b, "(%03d)", tree->n);
-	left = print_t(tree->left, offset, depth + 1, s);
-	right = print_t(tree->right, offset + left + width, depth + 1, s);
-	for (i = 0; i < width; i++)
-		s[depth][offset + left + i] = b[i];
-	if (depth && is_left)
-	{
-		for (i = 0; i < width + right; i++)
-			s[depth - 1][offset + left + width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	else if (depth && !is_left)
-	{
-		for (i = 0; i < left + width; i++)
-			s[depth - 1][offset - width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	return (left + width + right);
+	return (1 + binary_tree_size(root->left) + binary_tree_size(root->right));
 }
 
 /**
- * _height - func to measure the height of a binary tree
- *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
+ * is_complete_recursive - Recursive helper function
+ * @root: pointer to the root of the node
+ * @index: Index of the current node
+ * @size: total number of node in the tree
+ * Return: 1 if the tree is complete || 0 on failure
  */
-static size_t _height(const binary_tree_t *tree)
-{
-	size_t height_l;
-	size_t height_r;
 
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+int is_complete_recursive(const binary_tree_t *root, size_t index, size_t size)
+{
+	if (root == NULL)
+		return (1);
+
+	if (index >= size)
+		return (0);
+
+	return (is_complete_recursive(root->left, 2 * index + 1, size) &&
+		is_complete_recursive(root->right, 2 * index + 2, size));
 }
 
 /**
- * binary_tree_print - function that prints a binary tree
- *
- * @tree: Pointer to the root node of the tree to be printed
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: pointer to the node
+ * Return: 0 if tree is NULL
  */
-void binary_tree_print(const binary_tree_t *tree)
-{
-	char **s;
-	size_t height, i, j;
 
-	if (!tree)
-		return;
-	height = _height(tree);
-	s = malloc(sizeof(*s) * (height + 1));
-	if (!s)
-		return;
-	for (i = 0; i < height + 1; i++)
-	{
-		s[i] = malloc(sizeof(**s) * 255);
-		if (!s[i])
-			return;
-		memset(s[i], 32, 255);
-	}
-	print_t(tree, 0, 0, s);
-	for (i = 0; i < height + 1; i++)
-	{
-		for (j = 254; j > 1; --j)
-		{
-			if (s[i][j] != ' ')
-				break;
-			s[i][j] = '\0';
-		}
-		printf("%s\n", s[i]);
-		free(s[i]);
-	}
-	free(s);
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (tree == NULL)
+		return (0);
+
+	size = binary_tree_size(tree);
+	return (is_complete_recursive(tree, 0, size));
 }
